6030cf54b9d98a4d42271007feda2302ed47426e
Tim Wade <twade@peachtreebilling.com>
2014-08-01 08:52:42 -0400

    First iteration.

--- app.rb
+++ app.rb
@@ -1,23 +1,83 @@
 require 'sinatra'
 
+class Page
+  extend Enumerable
+
+  def self.each(&block)
+    all.each(&block)
+  end
+
+  def self.build(opts)
+    new(opts).save
+  end
+
+  def self.all
+    @all ||= []
+  end
+
+  attr_accessor :page_id, :content
+
+  def initialize(opts)
+    @page_id = opts.fetch('pageId')
+    @content = opts.fetch('content')
+  end
+
+  def save
+    Page.all << self
+  end
+end
+
+class Boogler
+  attr_accessor :query, :matches
+
+  def initialize(query)
+    @query = query
+    @matches = []
+    search
+  end
+
+  def search_terms
+    @search_terms ||= query.gsub(/[^a-zA-Z\s]/, '').split(/\s+/).map(&:downcase).uniq
+  end
+
+  def search
+    search_terms.each do |term|
+      pages_with_term(term).map(&:page_id).each do |pid|
+        increment_score_for_page(pid)
+      end
+    end
+  end
+
+  def increment_score_for_page(pid)
+    if found_match = matches.detect { |match| match['pageId'] == pid }
+      found_match['score'] += 1
+    else
+      matches << {'pageId' => pid, 'score' => 1}
+    end
+  end
+
+  def pages_with_term(term)
+    Page.select { |page| page.content.downcase[term] }
+  end
+
+  def to_json
+    {'matches' => matches}.to_json
+  end
+end
+
 get '/search' do
   content_type :json
-  {
-    'matches' => [
-                  {
-                    'pageId' => 300,
-                    'score' => 3
-                  },
-
-                  {
-                    'pageId' => 12,
-                    'score' => 1
-                  }
-                 ]
-  }.to_json
+  Boogler.new(params['query']).to_json
 end
 
 post '/index' do
   content_type :json
-  status 204
+  params_json = JSON.parse(request.body.read)
+  @page = Page.new(params_json)
+
+  if @page.save
+    status 204
+  else
+    status 404
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -25,17 +25,35 @@ describe "boogle" do
   end
 
   describe "/index" do
-    specify "post" do
-      post "/index", example_data
-      expect(last_response.status).to eq(204)
+    describe "post" do
+      after { Page.all.clear }
+
+      it "accepts a request with valid params" do
+        post "/index", example_data
+        expect(last_response.status).to eq(204)
+      end
+
+      it "adds a page" do
+        expect {
+          post "/index", example_data
+        }.to change(Page, :count).by(1)
+      end
     end
   end
 
   describe "/search" do
-    specify "get" do
-      get "/search?query=Elementary,%20dear%20Watson"
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq(example_response)
+    describe "get" do
+      before :all do
+        Page.build('pageId' => 300, 'content' => 'Elementary, dear Watson')
+        Page.build('pageId' => 12, 'content' => 'Dear')
+      end
+
+      it "returns the results for the search" do
+        get "/search?query=Elementary,%20dear%20Watson"
+        expect(last_response.status).to eq(200)
+        expect(last_response).to be_ok
+        expect(last_response.body).to eq(example_response)
+      end
     end
   end
 
